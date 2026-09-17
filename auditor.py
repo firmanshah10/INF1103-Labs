b5dc017dc9a40510bbb2d5b50634971d905b24f1
Firman Shah <frenkiefir22@gmail.com>
2026-09-17 21:04:18 +0800

    added the while loop to ask for stock quantity & quit, and converting string input to integer

--- auditor.py
+++ auditor.py
@@ -1,5 +1,13 @@
 total = 0
 rejected = 0
 
+while True:
+    entry = input("Enter a stock quantity (or quit): ")
+    if entry == "quit":
+        break
+    if entry.isdigit():
+        int_entry = int(entry)
+        total += int_entry
+        
 
 
