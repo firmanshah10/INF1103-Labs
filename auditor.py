f4a3b9023dcf1481492ab82f8338aae2e847ca2f
Firman Shah <frenkiefir22@gmail.com>
2026-09-17 20:58:40 +0800

    1st

--- auditor.py
+++ auditor.py
@@ -1,18 +1,5 @@
 total = 0
 rejected = 0
 
-while True:
-    entry = input("Enter a stock quantity (or quit): ")
-    if entry == "quit":
-        break
-    if entry.isdigit():
-        integer_entry = int(entry)
-        total+= integer_entry
-        if total > 500:
-            print("You have exceeded the maximum stock limit of 500")
-            break
-    else:
-        print("Invalid input, please enter a whole number")
-        rejected +=1
-print("Total Stock Quantity:", total)
-print("Rejected Entries", rejected)
+
+
